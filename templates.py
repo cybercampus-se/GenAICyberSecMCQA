cda9ebc91b55768cc6e4c6537be8a989d5af4837
GKeppler <gustavkeppler@gmail.com>
2024-08-19 16:50:23 +0200

    cleanup

---
+++ templates.py
@@ -0,0 +1,203 @@
+MMLU_PRO = """The following are multiple choice questions (with answers) about computer security. Think step by step and then finish your answer with "the answer is (X)" where X is the correct letter choice.
+
+Question: {Exam_Question}
+{Exam_Choices}
+"""
+
+
+LLAMA31_INSTRUCT_MMLU_5_SHOT_OPENAI =  [
+
+   {"role": "user", "content": """Given the following question and four candidate answers (A, B, C and D), choose the best answer.
+Question: SHA-1 has a message digest of
+A. 160 bits
+B. 512 bits
+C. 628 bits
+D. 820 bits
+Your response should end with \"The best answer is [the_answer_letter]\" where the [the_answer_letter] is one of A, B, C or D."""},
+
+    {"role": "assistant", "content": "The best answer is A."},
+    {"role": "user", "content": """Given the following question and four candidate answers (A, B, C and D), choose the best answer.
+Question: _____________ can modify data on your system – so that your system doesn’t run correctly or you can no longer access specific data, or it may even ask for ransom in order to give your access.
+A. IM – Trojans
+B. Backdoor Trojans
+C. Trojan-Downloader
+D. Ransom Trojan
+Your response should end with \"The best answer is [the_answer_letter]\" where the [the_answer_letter] is one of A, B, C or D."""},
+
+   {"role": "assistant", "content": "The best answer is D."},
+   {"role": "user", "content": """Given the following question and four candidate answers (A, B, C and D), choose the best answer.
+Question: What is ethical hacking?
+A. \"Hacking\" ethics so they justify unintended selfish behavior
+B. Hacking systems (e.g., during penetration testing) to expose vulnerabilities so they can be fixed, rather than exploited
+C. Hacking into systems run by those whose ethics you disagree with
+D. A slang term for rapid software development, e.g., as part of hackathons
+Your response should end with \"The best answer is [the_answer_letter]\" where the [the_answer_letter] is one of A, B, C or D."""},
+
+   {"role": "assistant", "content": "The best answer is B."},
+   {"role": "user", "content": """Given the following question and four candidate answers (A, B, C and D), choose the best answer.
+Question: Exploitation of the Heartbleed bug permits
+A. overwriting cryptographic keys in memory
+B. a kind of code injection
+C. a read outside bounds of a buffer
+D. a format string attack
+Your response should end with \"The best answer is [the_answer_letter]\" where the [the_answer_letter] is one of A, B, C or D."""},
+
+   {"role": "assistant", "content": "The best answer is C."},
+   {"role": "user", "content": """Given the following question and four candidate answers (A, B, C and D), choose the best answer.
+Question: The ____________ is anything which your search engine cannot search.
+A. Haunted web
+B. World Wide Web
+C. Surface web
+D. Deep Web
+Your response should end with \"The best answer is [the_answer_letter]\" where the [the_answer_letter] is one of A, B, C or D."""},
+
+   {"role": "assistant", "content": "The best answer is D."},
+   {"role": "user", "content": """Given the following question and four candidate answers (A, B, C and D), choose the best answer.
+Question: {Exam_Question}
+{Exam_Choices}
+Your response should end with \"The best answer is [the_answer_letter]\" where the [the_answer_letter] is one of A, B, C or D."""},
+{"role": "assistant", "content": "The best answer is"},
+]
+
+LLAMA31_INSTRUCT_CCNA_5_SHOT_OPENAI =  [
+
+   {"role": "user", "content": """The following are multiple choice questions (with answers) about network fundamentals, network access, security fundamentals, automation and programmability.
+Question: Which two options are the best reasons to use an IPV4 private IP space? (Choose two.)
+A. to enable intra-enterprise communication
+B. to implement NAT
+C. to connect applications
+D. to conserve global address space
+E. to manage routing overhead
+Your response should end with \"The best answer is [the_answer_letter(s)]\" where the [the_answer_letter(s)] is/are of A, B, C, D, E, F,..."""},
+
+    {"role": "assistant", "content": "The best answer is AD."},
+    {"role": "user", "content": """The following are multiple choice questions (with answers) about network fundamentals, network access, security fundamentals, automation and programmability.
+Question: Security Group Access requires which three syslog messages to be sent to Cisco ISE? (Choose three.)
+A. IOS-7-PROXY_DROP
+B. AP-1-AUTH_PROXY_DOS_ATTACK
+C. MKA-2-MACDROP
+D. AUTHMGR-5-MACMOVE
+E. ASA-6-CONNECT_BUILT
+F. AP-1-AUTH_PROXY_FALLBACK_REQ
+Your response should end with \"The best answer is [the_answer_letter(s)]\" where the [the_answer_letter(s)] is/are of A, B, C, D, E, F,..."""},
+
+   {"role": "assistant", "content": "The best answer is BDF."},
+   {"role": "user", "content": """The following are multiple choice questions (with answers) about network fundamentals, network access, security fundamentals, automation and programmability.
+Question: Which two authentication stores are supported to design a wireless network using PEAP EAP-MSCHAPv2 as the authentication method? (Choose two.)
+A. Microsoft Active Directory
+B. ACS
+C. LDAP
+D. RSA Secure-ID
+E. Certificate Server
+Your response should end with \"The best answer is [the_answer_letter(s)]\" where the [the_answer_letter(s)] is/are of A, B, C, D, E, F,..."""},
+
+   {"role": "assistant", "content": "The best answer is AB."},
+   {"role": "user", "content": """The following are multiple choice questions (with answers) about network fundamentals, network access, security fundamentals, automation and programmability.
+Question: The corporate security policy requires multiple elements to be matched in an authorization policy. Which elements can be combined to meet the requirement?
+A. Device registration status and device activation status
+B. Network access device and time condition
+C. User credentials and server certificate
+D. Built-in profile and custom profile
+Your response should end with \"The best answer is [the_answer_letter(s)]\" where the [the_answer_letter(s)] is/are of A, B, C, D, E, F,..."""},
+
+   {"role": "assistant", "content": "The best answer is B."},
+   {"role": "user", "content": """The following are multiple choice questions (with answers) about network fundamentals, network access, security fundamentals, automation and programmability.
+Question: Which three posture states can be used for authorization rules? (Choose three.)
+A. unknown
+B. known
+C. noncompliant
+D. quarantined
+E. compliant
+F. no access
+G. limited
+Your response should end with \"The best answer is [the_answer_letter(s)]\" where the [the_answer_letter(s)] is/are of A, B, C, D, E, F,..."""},
+
+   {"role": "assistant", "content": "The best answer is ACE."},
+   {"role": "user", "content": """The following are multiple choice questions (with answers) about network fundamentals, network access, security fundamentals, automation and programmability.
+Question: {Exam_Question}
+{Exam_Choices}
+Your response should end with \"The best answer is [the_answer_letter(s)]\" where the [the_answer_letter(s)] is/are of A, B, C, D, E, F,..."""},
+{"role": "assistant", "content": "The best answer is"},
+]
+
+LLAMA31_INSTRUCT_CC_5_SHOT_OPENAI =  [
+
+   {"role": "user", "content": """The following are multiple choice questions (with answers).
+Question: Which two options are the best reasons to use an IPV4 private IP space? (Choose two.)
+A. to enable intra-enterprise communication
+B. to implement NAT
+C. to connect applications
+D. to conserve global address space
+E. to manage routing overhead
+Your response should end with \"The best answer is [the_answer_letter(s)]\" where the [the_answer_letter(s)] is/are of A, B, C, D, E, F,..."""},
+
+    {"role": "assistant", "content": "The best answer is AD."},
+    {"role": "user", "content": """The following are multiple choice questions (with answers).
+Question: Security Group Access requires which three syslog messages to be sent to Cisco ISE? (Choose three.)
+A. IOS-7-PROXY_DROP
+B. AP-1-AUTH_PROXY_DOS_ATTACK
+C. MKA-2-MACDROP
+D. AUTHMGR-5-MACMOVE
+E. ASA-6-CONNECT_BUILT
+F. AP-1-AUTH_PROXY_FALLBACK_REQ
+Your response should end with \"The best answer is [the_answer_letter(s)]\" where the [the_answer_letter(s)] is/are of A, B, C, D, E, F,..."""},
+
+   {"role": "assistant", "content": "The best answer is BDF."},
+   {"role": "user", "content": """The following are multiple choice questions (with answers).
+Question: Which two authentication stores are supported to design a wireless network using PEAP EAP-MSCHAPv2 as the authentication method? (Choose two.)
+A. Microsoft Active Directory
+B. ACS
+C. LDAP
+D. RSA Secure-ID
+E. Certificate Server
+Your response should end with \"The best answer is [the_answer_letter(s)]\" where the [the_answer_letter(s)] is/are of A, B, C, D, E, F,..."""},
+
+   {"role": "assistant", "content": "The best answer is AB."},
+   {"role": "user", "content": """The following are multiple choice questions (with answers).
+Question: The corporate security policy requires multiple elements to be matched in an authorization policy. Which elements can be combined to meet the requirement?
+A. Device registration status and device activation status
+B. Network access device and time condition
+C. User credentials and server certificate
+D. Built-in profile and custom profile
+Your response should end with \"The best answer is [the_answer_letter(s)]\" where the [the_answer_letter(s)] is/are of A, B, C, D, E, F,..."""},
+
+   {"role": "assistant", "content": "The best answer is B."},
+   {"role": "user", "content": """The following are multiple choice questions (with answers).
+Question: Which three posture states can be used for authorization rules? (Choose three.)
+A. unknown
+B. known
+C. noncompliant
+D. quarantined
+E. compliant
+F. no access
+G. limited
+Your response should end with \"The best answer is [the_answer_letter(s)]\" where the [the_answer_letter(s)] is/are of A, B, C, D, E, F,..."""},
+
+   {"role": "assistant", "content": "The best answer is ACE."},
+   {"role": "user", "content": """The following are multiple choice questions (with answers).
+Question: {Exam_Question}
+{Exam_Choices}
+Your response should end with \"The best answer is [the_answer_letter(s)]\" where the [the_answer_letter(s)] is/are of A, B, C, D, E, F,..."""},
+{"role": "assistant", "content": "The best answer is"},
+]
+
+LLAMA31_INSTRUCT_CC_0_SHOT_COT = """The following are multiple choice questions (with answers), choose the best answer.
+
+Question: {Exam_Question}
+{Exam_Choices}
+
+- For simple problems:
+Directly provide the answer with minimal explanation.
+
+- For complex problems:
+Use this step-by-step format:
+## Step 1: [Concise description]
+[Brief explanation]
+## Step 2: [Concise description]
+[Brief explanation]
+
+Regardless of the approach, always conclude with:
+The best answer is [the_answer_letter(s)].
+where the [the_answer_letter(s)] is one or multiple of A, B, C, D, E, F...
+
+Let's think step by step."""
---
+++ utils.py
@@ -0,0 +1,214 @@
+import pandas as pd
+import re
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import random
+import yaml
+
+########################################
+#Functions for the evaluation of the LLM
+########################################
+
+def load_config(config_paht):
+    """Loads the LLM model configuration from the provided path.
+
+    Args:
+        config_path: The path to the LLM model configuration file.
+
+    Returns:
+        The LLM model configuration.
+    """
+    with open(config_paht, 'r') as f:
+        
+        config = yaml.safe_load(f)
+    return config
+
+
+def extract_answer(answer):
+    """Extracts the correct answers from the provided answer string.
+
+    Args:
+        answer: The answer string to extract the correct answers from.
+
+    Returns:
+        A list of correct answers. 
+    """
+
+    # Cleaning the input by removing some non-relevant characters
+    answer_proc = re.sub(r'[\s\n.,]', '', answer)
+    
+    # Define regex patterns for different cases
+    pattern_single_letters = re.compile(r'^[A-J]+$')
+    #pattern1 = re.compile(r"answer is \(?([A-J]+)\)?", re.IGNORECASE)
+    pattern1 = re.compile(r"answer is \[?([A-J]+)\]?", re.IGNORECASE)
+    pattern2 = re.compile(r'.*[aA]nswer:\s*([A-J]+)', re.IGNORECASE)
+    
+    if re.match(pattern_single_letters, answer_proc):
+        return list(answer_proc)
+    else:
+        # Find matches using the first regex pattern
+        #drop , from answer
+
+        match1 = pattern1.findall(answer)
+        
+        # Find matches using the second regex pattern
+        match2 = pattern2.findall(answer)
+        
+        # Combine results from both patterns
+        results = match1 + match2
+        
+        # Flatten the list and remove duplicates
+        combined_results = []
+        for result in results:
+            combined_results.extend(list(result))
+        
+        return list(set(combined_results))
+    
+def compare_answers(answerLLM, answer_exam):
+    """Compares the extracted correct answers with the answers in answer_exam.
+
+    Keyword arguments:
+    answerLLM -- the list of answers extracted from the LLM answer
+    answer_exam -- list of answers from the exam
+    """
+    # Convert answer_exam_list from letters to numbers
+    answerLLM = [ord(answer) - 65 for answer in answerLLM]
+
+    # Get number of correct answers in the exam
+    num_of_correct_exam_answers = len(answer_exam)
+
+    # Convert both lists to sets for efficient comparison
+    answer_LLM_set = set(answerLLM)
+    answer_exam_set = set(answer_exam)
+
+    # Calculate the count of matching answers
+    number_of_correct_llm_answers = len(answer_LLM_set.intersection(answer_exam_set))
+
+    #Calculate the number of incorrect answers
+    number_of_incorrect_llm_answers = len(answer_LLM_set.difference(answer_exam_set))
+
+    # Check if the number of answers given by the LLM is greater than the number of correct answers
+    too_many_answ_given = False
+    if len(answer_LLM_set) > num_of_correct_exam_answers:
+        too_many_answ_given = True
+
+    # Return a dictionary with the matching count and the number of correct answers
+    return number_of_correct_llm_answers, too_many_answ_given, number_of_incorrect_llm_answers
+
+def format_choices_for_llm(choices):
+    #Define the letters for the choices
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    
+    # Erstellen Sie den formatierten String
+    formatted_choices = '\n'.join(f'{letters[i]}. {choice}' for i, choice in enumerate(choices))
+    
+    return formatted_choices
+
+def evaluation_sampling(llm_answer, exam_Answers, num_of_correct_answer):
+    """Analyse the answer given by the LLM and compare it with the exam answers.
+
+    Keyword arguments:
+    llm_answer -- the answer string given by the LLM
+    exam_Answers -- the list of answers from the exam
+    """
+
+    answerLLM = extract_answer(llm_answer)
+    if answerLLM is not None:
+        num_of_correct_llm_Answers, too_many_answ, number_of_incorrect_llm_answers = compare_answers(answerLLM, exam_Answers)
+        if num_of_correct_llm_Answers == num_of_correct_answer and too_many_answ == False:
+            answered_correctly = True
+        else:
+            answered_correctly = False 
+        return num_of_correct_llm_Answers, answerLLM, too_many_answ, answered_correctly, number_of_incorrect_llm_answers
+    else:
+         return -1
+
+
+def evaluation(llm_output_dataframe):
+
+    # Compute the number of total questions for each model
+    number_of_questions = llm_output_dataframe.groupby('Model')['QuestionIndex'].count()
+    
+    #Number of fully correct answers given by the LLM
+    correctly_answered = llm_output_dataframe.groupby('Model')['Answered_Correctly'].sum()
+
+    #Number of incorrect answers given by the LLM
+    incorrectly_answered = number_of_questions - correctly_answered
+
+    #Amount of correct answers in the exam
+    amount_correct_exam_answers = llm_output_dataframe.groupby('Model')['NumberOfCorrectExamAnswers'].sum()
+
+    #Amount of correct answers given by the LLM even if not fully correct
+    amount_correct_llm_answers = llm_output_dataframe.groupby('Model')['NumberOfCorrectLLMAnswers'].sum()
+    
+    # Calculate Partial Credits
+    llm_output_dataframe['Partial_Credit'] = llm_output_dataframe.apply(
+        lambda row: max(0, row['NumberOfCorrectLLMAnswers'] / row['NumberOfCorrectExamAnswers'] - 
+                        (row['NumberOfIncorrectLLMAnswers'] /row['NumberOfCorrectExamAnswers'])), axis=1)
+    
+    # Aggregate Partial Credit for each model
+    partial_credit_sum = llm_output_dataframe.groupby('Model')['Partial_Credit'].sum()
+
+    #Calculation of Accuracy and Recall and f1 score
+    accuracy = correctly_answered / number_of_questions
+    accuracy_partial = partial_credit_sum / number_of_questions    
+    
+    results_df = pd.DataFrame({
+        'Number of Questions': number_of_questions,
+        'Correctly Answered': correctly_answered,
+        'Incorrectly Answered': incorrectly_answered,
+        'Accuracy': accuracy,
+        'Accuracy Partial': accuracy_partial,
+        'Total Partial Credit': partial_credit_sum
+    })
+
+    results_df = results_df.reset_index()
+
+    return results_df
+
+
+def calculate_model_statistics(df):
+    """
+    Calculates statistics for each model in the DataFrame.
+    
+    Args:
+    df (DataFrame): Input DataFrame containing evaluation metrics for different models.
+    
+    Returns:
+    DataFrame: New DataFrame containing calculated statistics for each model.
+    """
+    model_stats = []
+    for model, group_df in df.groupby('Model'):
+        model_stat = {
+            'Model': model,
+            'Accuracy Mean': group_df['Accuracy'].mean(),
+            'Accuracy Max': group_df['Accuracy'].max(),
+            'Accuracy Min': group_df['Accuracy'].min(),
+            'Accuracy STD': group_df['Accuracy'].std(),
+            'Accuracy Partial Mean': group_df['Accuracy Partial'].mean(),
+            'Accuracy Partial Max': group_df['Accuracy Partial'].max(),
+            'Accuracy Partial Min': group_df['Accuracy Partial'].min(),
+            'Accuracy Partial STD': group_df['Accuracy Partial'].std()
+        }
+        model_stats.append(model_stat)
+    
+    return pd.DataFrame(model_stats)
+
+
+def shuffle_choices_and_update_answer(choices, answer, seed):
+    # Erstellen Sie eine Liste von Indizes und mischen Sie sie
+    indices = list(range(len(choices)))
+    random.Random(seed).shuffle(indices)
+    shuffled_choices = [choices[i] for i in indices]
+    updated_answer = [indices.index(a) for a in answer]  
+    
+    return shuffled_choices, updated_answer
+
+if __name__ == "__main__":
+    print(extract_answer("The answer is ABCDE."))
+    print(extract_answer("The best answer is C, E."))
+    print(extract_answer("The best answer is [DE]. The access and distribution layers must be on the same device"))
+    print(shuffle_choices_and_update_answer(["0","1","2","3","5"],[1],1))
+    print(shuffle_choices_and_update_answer(["0","1","2","3","5"],[1],2))
+    print(shuffle_choices_and_update_answer(["0","1","2","3","5"],[1],3))
+    print(shuffle_choices_and_update_answer(["0","1","2","3","5"],[1],4))
